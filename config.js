204796e31026e08050821c280c4951444eb753a2
TuiLaPhat <114066684+TuiLaPhat@users.noreply.github.com>
2022-09-21 17:12:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "TẶNG BÀ XÃ NÈ",
+    introTitle: 'xundien oiiiii',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
